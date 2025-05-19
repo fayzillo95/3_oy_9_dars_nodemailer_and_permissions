2e3ba2ad7c2e0c8e1a97510379f6de57bc3255b3
fayzillo95 <ovovovlululutvata@gmail.com>
2025-05-20 02:50:30 +0500

    adding log

--- src/midllwares/verfyToken.js
+++ src/midllwares/verfyToken.js
@@ -8,8 +8,8 @@ export const verfyToken = (req, res, next) => {
         const clientAgent = req.headers['user-agent']
 
         const decoded = JWT.verify(token, process.env.JWT_ACCESS_KEY)
-        console.log("Verfy token line 11 ",decoded)
-        if (decoded.ip && decoded.ip != clientIP) {
+        console.log("Verfy token line 11 ",decoded.ip , clientIP)
+        if (decoded.ip != clientIP) {
             throw new AuthorizationError("IP address mismatch", 406)
         }
 
